f3f5876086f7d4db4635fd9da1d69205bcaacf13
julianu37 <julianuddtank@gmail.com>
2025-04-29 18:55:07 -0300

    Remoção do card de "Manuais" da view welcome.blade.php, simplificando a interface. Comentários foram adicionados para facilitar o entendimento do código.

--- storage/framework/views/53d797b718f2ebd04c71b4a45dc4b10c.php
+++ storage/framework/views/53d797b718f2ebd04c71b4a45dc4b10c.php
@@ -48,14 +48,6 @@
                 
                 
                 
-                <a href="<?php echo e(route('manuais.index')); ?>" class="block p-6 bg-white dark:bg-gray-800 rounded-lg shadow hover:shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1">
-                     <div class="flex items-center mb-3">
-                        
-                        <svg class="w-8 h-8 text-blue-500 dark:text-blue-400 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
-                        <h3 class="text-xl font-semibold text-gray-800 dark:text-gray-100">Manuais</h3>
-                    </div>
-                    <p class="text-sm text-gray-600 dark:text-gray-400">Faça o download de manuais de serviço e de usuário.</p>
-                </a>
                  
                  <a href="<?php echo e(route('marcas.index')); ?>" class="block p-6 bg-white dark:bg-gray-800 rounded-lg shadow hover:shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1">
                      <div class="flex items-center mb-3">
